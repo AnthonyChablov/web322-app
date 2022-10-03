49585efc3b5fcbd3f88d3f88820da83cb0a58796
Anthony Chablov <achablov@myseneca.ca>
2022-10-03 14:17:06 -0400

    add routes and data-service

---
+++ data-service.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+let departments = [];
+let employees = [];
+
+const initalize = async () => {
+    try{
+        const data = await fs.promises.readFile(__dirname + '/data/employees.json', "utf-8");
+        employees = JSON.parse(data);
+
+        try{
+            const data = await fs.promises.readFile(__dirname + '/data/departments.json', "utf-8");
+            departments = JSON.parse(data);
+        }catch{
+            console.log(`${error} unable to read file`);
+        }
+        
+    }catch(error){
+        console.log(`${error} unable to read file`);
+    }
+}
+
+const getAllEmployees = () =>{
+    return new Promise((resolve, reject)=>{
+        employees.length === 0 
+            ? reject("no results returned") 
+            : resolve(employees);
+    })
+}
+
+const getDepartments = () =>{
+    return new Promise((resolve, reject)=>{
+        departments.length === 0 
+            ? reject("no results returned") 
+            : resolve(departments);
+    })
+}
+
+const getManagers = () =>{
+    return new Promise((resolve, reject)=>{
+        let findManagers = employees.filter((employee)=>employee.isManager)
+        !findManagers
+            ? reject("no results returned") 
+            : resolve(findManagers)
+    })
+}
+
+module.exports = {
+    initalize,
+    getManagers,
+    getDepartments, 
+    getAllEmployees, 
+    departments, 
+    employees
+ }
---
+++ server.js
@@ -0,0 +1,54 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: __________Anthony Chablov______ Student ID: __158794214__ Date: __October 3, 2022_________
+*
+*  Online (Heroku) Link: ________________________________________________________
+*
+********************************************************************************/ 
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const dataService = require('./data-service');
+
+// middleware
+app.use(express.static('public'));
+
+//routes
+app.get(('/'), (req,res) => {
+    res.sendFile(path.join( __dirname, "/views/home.html" ));
+});
+
+app.get(('/about'), (req,res) => {
+    res.sendFile(path.join( __dirname, "/views/about.html" ));
+});
+
+app.get(('/employees'), (req,res) => {
+    dataService.getAllEmployees()
+        .then((data) => res.json(data))
+        .catch((err) => { return {"message" : err} })
+});
+
+app.get(('/departments'), (req,res)=>{
+    dataService.getDepartments()
+        .then((data) => res.json(data))
+        .catch((err) => { return {"message" : err} })
+});
+
+app.get(('/managers'), (req,res) => {
+    dataService.getManagers()
+        .then((data) => res.json(data))
+        .catch((err) => { return {"message" : err} })
+});
+
+app.get('*', (req, res) => {
+    res.send("Page Not Found.", 404);
+});
+
+dataService.initalize()
+    .then(() => app.listen(process.env.PORT | 8080, () => console.log("Express http server listening on 8080")))
+        .catch((err) => console.log(`${err}, Error has occured`))
+
